2082d77ea60b995e5209f1da4b0432206ac309c9
pierredes <pierredesombre.pro@gmail.com>
2020-11-12 16:23:02 +0100

    add exports

--- models/sauce.js
+++ models/sauce.js
@@ -13,3 +13,5 @@ const sauce = mongoose.Schema({
     usersLiked: {type: [String], required: true},
     usersDisliked: {type: [String], required: true},
 });
+
+module.exports = mongoose.model('Sauce', sauce);
