2fcd5f10c85fd5ff51a7e1b92518baeb4a9b6c58
pierredes <pierredesombre.pro@gmail.com>
2020-11-17 15:14:14 +0100

    add authentification on all route

--- routes/sauce.js
+++ routes/sauce.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+const authentification = require('../middleware/auth.js');
 const controlleurSauce = require('../controleur/sauce.js');
 
 
-router.post('/', controlleurSauce.creerSauce);
-router.put('/:id', controlleurSauce.modifierSauce);
-router.delete('/:id', controlleurSauce.supprimerUneSauce);
-router.get('/:id', controlleurSauce.trouverUneSauce);
-router.get('/', controlleurSauce.trouverTouteLesSauces);
+router.post('/', authentification, controlleurSauce.creerSauce);
+router.put('/:id', authentification, controlleurSauce.modifierSauce);
+router.delete('/:id', authentification, controlleurSauce.supprimerUneSauce);
+router.get('/:id', authentification, controlleurSauce.trouverUneSauce);
+router.get('/', authentification, controlleurSauce.trouverTouteLesSauces);
 
 module.exports = router;
