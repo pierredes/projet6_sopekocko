64a910866d97374076a2edb0babed381f406766c
pierredes <pierredesombre.pro@gmail.com>
2020-11-17 08:40:14 +0100

    add validator

--- models/user.js
+++ models/user.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const userValidator = require('mongoose-unique-validator');
 
 const user = mongoose.Schema({
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
 });
+
+user.plugin(userValidator);
 
 module.exports = mongoose.model('User', user);
