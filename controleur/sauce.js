5bde8431ba9dea78b43172fed23103efbbafee4d
pierredes <pierredesombre.pro@gmail.com>
2020-11-17 17:18:12 +0100

    modify post, put and delete for include file gestion

--- controleur/sauce.js
+++ controleur/sauce.js
@@ -1,9 +1,16 @@
 const Sauce = require('../models/sauce.js');
+const fs = require('fs');
 
 exports.creerSauce = (req, res, next) => {
-    delete req.body._id; 
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id; 
     const sauce = new Sauce({
-        ...req.body
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes: '0',
+        dislikes: '0',
+        usersLiked: [],
+        usersDisliked: []
     });
     sauce.save()
     .then(() => res.status(201).json({ message: 'Sauce crée'}))
@@ -11,15 +18,28 @@ exports.creerSauce = (req, res, next) => {
 };
 
 exports.modifierSauce = (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id}, { ...req.body, _id: req.params.id})
+    const sauceObject = req.file ?
+    {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
     .then(() => res.status(200).json({message: 'Sauce modifié !'}))
     .catch(error => res.status(400).json({error}));
 };
 
 exports.supprimerUneSauce = (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id})
-    .then(() => res.status(200).json({message: 'Sauce supprimé !'}))
-    .catch(error => res.status(400).json({error}));
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () =>{
+            Sauce.deleteOne({ _id: req.params.id})
+            .then(() => res.status(200).json({message: 'Sauce supprimé !'}))
+            .catch(error => res.status(400).json({error}));
+        })
+    })
+    .catch(error => res.status(500).json({error}));
+    
 };
 
 exports.trouverUneSauce = (req, res, next) => {
