3d5d5963cecf371e2a8169adc190d655dd32594e
pierredes <pierredesombre.pro@gmail.com>
2020-11-17 17:16:52 +0100

    add image gestion

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const sauceRoute = require('./routes/sauce.js');
 const userRoute = require('./routes/user.js');
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/sauces', sauceRoute);
 app.use('/api/auth', userRoute);
