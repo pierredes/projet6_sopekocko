95ac21f62e1292e9b85d245525e03dd2e195752a
pierredes <pierredesombre.pro@gmail.com>
2020-12-01 13:37:43 +0100

    add multer

--- routes/sauce.js
+++ routes/sauce.js
@@ -3,11 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const authentification = require('../middleware/auth.js');
+const multer = require('../middleware/multer-config.js');
 const controlleurSauce = require('../controleur/sauce.js');
 
 
-router.post('/', authentification, controlleurSauce.creerSauce);
-router.put('/:id', authentification, controlleurSauce.modifierSauce);
+router.post('/', authentification, multer, controlleurSauce.creerSauce);
+router.post('/:id/like', authentification, controlleurSauce.likeSauce);
+router.put('/:id', authentification, multer, controlleurSauce.modifierSauce);
 router.delete('/:id', authentification, controlleurSauce.supprimerUneSauce);
 router.get('/:id', authentification, controlleurSauce.trouverUneSauce);
 router.get('/', authentification, controlleurSauce.trouverTouteLesSauces);
